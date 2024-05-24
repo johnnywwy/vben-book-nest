797ccc814f6e376d9b63f47acc8e657330c26a23
wuwenyu <johnnywwy@gmail.com>
2024-05-24 18:17:55 +0800

    chore：book模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { BookModule } from './modules/book/book.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
