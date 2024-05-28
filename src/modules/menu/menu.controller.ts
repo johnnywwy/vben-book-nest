5953f19a04581b73763b27e4a8d8765b55809cf3
wuwenyu <johnnywwy@gmail.com>
2024-05-28 15:07:52 +0800

    feat: 实现后端返回路由

--- src/modules/menu/menu.controller.ts
+++ src/modules/menu/menu.controller.ts
@@ -1,33 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Public } from '../auth/public.decorator';
+import { wrapperResponse } from 'src/utils';
 
+@ApiTags('菜单管理')
 @Controller('menu')
 export class MenuController {
   constructor(private readonly menuService: MenuService) {}
 
   @Post()
+  @ApiOperation({ summary: '创建新菜单' })
   create(@Body() createMenuDto: CreateMenuDto) {
     return this.menuService.create(createMenuDto);
   }
 
   @Get()
+  @Public()
+  @ApiOperation({ summary: '获取所有菜单' })
   findAll() {
-    return this.menuService.findAll();
+    // return this.menuService.findAll();
+
+    return wrapperResponse(this.menuService.findAll(), '获取所有菜单');
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '获取单个菜单' })
   findOne(@Param('id') id: string) {
     return this.menuService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: '更新菜单' }) // 更新菜单
   update(@Param('id') id: string, @Body() updateMenuDto: UpdateMenuDto) {
     return this.menuService.update(+id, updateMenuDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: '删除菜单' })
   remove(@Param('id') id: string) {
     return this.menuService.remove(+id);
   }
